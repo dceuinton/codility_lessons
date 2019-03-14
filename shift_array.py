e4abf3a07ec85d29d4c1b20bda1b3c8891ecd4f7
Dale Euinton <dale.euinton@gmail.com>
2019-03-15 07:06:09 +1300

    Added the cyclic rotation challenge in python and tests.

--- shift_array.py
+++ shift_array.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3 
 
 def cyclic_rotate_array(arr, N):
-	return []
+	
+	if len(arr) is 0:
+		return []
+
+	length = len(arr)
+	shift = length - (N % length)
+
+	second_arr = arr[shift:]
+
+	for i in range(shift):
+		second_arr.append(arr[i])
+
+	return second_arr
--- test_shift_array.py
+++ test_shift_array.py
@@ -3,14 +3,25 @@ import shift_array
 
 class test_binary_gap(unittest.TestCase):
 
-	def test_shift_array(self):
-		self.assertEqual(shift_array.cyclic_rotate_array([], 2), [])
+	def test_cyclic_rotate_empty_array(self):
+		self.assertEqual(shift_array.cyclic_rotate_array([], 1), [])
+
+	def test_cyclic_rotate_single_element_array(self):
 		self.assertEqual(shift_array.cyclic_rotate_array([1], 1), [1])
+
+	def test_cyclic_rotate_single_shift(self):
 		self.assertEqual(shift_array.cyclic_rotate_array([1, 2], 1), [2, 1])
-		self.assertEqual(shift_array.cyclic_rotate_array([1, 2], 2), [1, 2])
+
+	def test_cyclic_rotate_to_original_array(self):
 		self.assertEqual(shift_array.cyclic_rotate_array([1, 2], 2), [1, 2])
 
+	def test_cyclic_rotate_more_than_size_of_array(self):
+		self.assertEqual(shift_array.cyclic_rotate_array([1, 2], 3), [2, 1])
+
+	def test_cyclic_rotate_array_of_same_values(self):
 		self.assertEqual(shift_array.cyclic_rotate_array([0, 0, 0], 1), [0, 0, 0])		
+
+	def test_cyclic_rotate_full_example(self):
 		self.assertEqual(shift_array.cyclic_rotate_array([3, 8, 9, 7, 6], 3), [9, 7, 6, 3, 8])		
 
 
