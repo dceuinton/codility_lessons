910232c0608101a171a7b65ff2e05eb91b007f28
Dale Euinton <dale.euinton@gmail.com>
2019-03-23 09:13:35 +1300

    Modified solution

--- binary_gap.py
+++ binary_gap.py
@@ -3,27 +3,24 @@
 # My solution to the codility challange of finding the binary gap in a number 
 
 def solution(N):
-	check = 2**31 -1
-	if N < 1 or N > check:
-		return -1
+    
+    count = 0
+    nOnes = 0
+    currentMax = 0
+    
+    for i in range(32):
+        if (N >> i & 1): # If it's a 1
+            nOnes += 1
+            
+            if nOnes > 1 and count > currentMax: 
+                currentMax = count
+            count = 0
+        
+        else:            # If it's a 0
+            if nOnes >= 1: 
+                count += 1
 
-	count = 0
-	num1s = 0
-	current_max = 0
-	for i in range(32):
-		if ((N >> i) & 1):
-			num1s += 1
-			if num1s % 2 is 0:
-				if count > current_max:
-					current_max = count
-				count = 0
-		elif num1s % 2 is 1:
-			count += 1
-
-	if num1s > 1: 
-		return current_max
-	else:
-		return 0
+    return currentMax
 
 def run_solution(N):
 	print("Num:", N, "binary:", bin(N))
@@ -31,9 +28,11 @@ def run_solution(N):
 
 
 
-# run_solution(1)
-# run_solution(4)
-# run_solution(5)
+run_solution(1)
+run_solution(4)
+run_solution(5)
+run_solution(15)
+run_solution(1041)
 # run_solution(31)
 # run_solution(33)
 # run_solution(49)
